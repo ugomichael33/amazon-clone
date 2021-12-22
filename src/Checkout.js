b1c980215db19b2aa0b12d7803dcfcbcdea9d433
ugochukwu onyekanma <ugochikanele@gmail.com>
2021-12-22 10:46:07 +0100

    third commit

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Checkout.css'
 import Subtotal from './Subtotal'
+import {useStateValue} from './StateProvider'
+import CheckoutProduct from './CheckoutProduct'
 
 function Checkout() {
+    const [{basket}, dispatch] = useStateValue()
     return (
         <div className='checkout'>
           <div className= 'checkout_left'>
@@ -14,6 +17,15 @@ function Checkout() {
                <h2 className='checkout_title'>
                  Your Shopping basket 
                </h2>
+               {basket.map(item => (
+                   <CheckoutProduct
+                   id={item.id}
+                   title={item.title}
+                   image={item.image}
+                   price={item.price}
+                   rating={item.rating}
+                   />
+               ))}
             </div>
           </div>  
           <div className='checkout_right'>
--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,22 @@
 import React from 'react'
 import './Product.css'
+import {useStateValue} from './StateProvider'
 
-function Products({title, image, price, rating}) {
+function Products({id, title, image, price, rating}) {
+  const [{basket} , dispatch]  = useStateValue()
+  
+  const addToBasket = () => {
+        dispatch({
+          type: 'ADD_TO_BASKET',
+          item: {
+            id: id,
+            title: title,
+            image: image,
+            price: price,
+            rating: rating
+          }
+        })
+    }
     return (
         <div className='product'>
           <div>
@@ -19,7 +34,7 @@ function Products({title, image, price, rating}) {
             </div>
           </div>  
           <img src={image}/>
-          <button>Add to basket</button>
+          <button onClick={addToBasket}>Add to basket</button>
         </div>
     )
 }
